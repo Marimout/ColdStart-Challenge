f747e786cc313def7691dddf6e5323c0ba69650f
marimout <truong.quocanh@outlook.com>
2021-04-03 16:17:57 +0100

    Challenge 3 - revert back to queue to store orders

--- api/orders-post/index.js
+++ api/orders-post/index.js
@@ -1,7 +1,18 @@
-const { postOrder } = require('../shared/order');
+const { getUser } = require('../shared/user-utils');
+const { config} = require('../shared/config');
+const { QueueClient, QueueServiceClient } = require("@azure/storage-queue");
 
 module.exports = async function (context, req) {
-  await postOrder(req.body);
+  // Get the user details from the request
+  const user = getUser(req);
+
+  // TODO: add the pre-order JSON document in a queue
+  const queueServiceClient = QueueServiceClient.fromConnectionString(config.azure_storage_connectionstring);
+  const queueClient = queueServiceClient.getQueueClient('preorders');
+
+  const base64Encode = (str) => Buffer.from(str).toString('base64')
+
+  await queueClient.sendMessage(base64Encode(JSON.stringify(req.body)));
 
   context.res.status(201);
 };
